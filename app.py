aa346e6ad04bceca97b52cd926b1693e6ffcda35
Amber Bhanra <amberbhamra19900@gmail.com>
2025-07-20 22:02:38 -0400

    done

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 command= ""
-while command != "quit":
+started= False
+while True:
     command=input("> ").lower()
     if command == "start":
-        print ("car started...")
+        if started:
+            print("car is already started!")
+        else:
+            started = True
+            print ("car started...")
     elif command == "stop":
+        if not started:
+            print ("car is already stopped.")
+        else:
+            started=False
         print("car stopped.")
     elif command =="help":
         print (""""
-        start- to start the car 
-        stop - to stop the car 
-        quit - to quit
+start- to start the car 
+stop - to stop the car 
+quit - to quit
         """)
+    elif command == "quit":
+        break
     else:
         print("sorry, i don't understand that ")
 
